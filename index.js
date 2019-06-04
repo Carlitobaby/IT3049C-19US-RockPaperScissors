3a7a1af4c1100f727b6d5fe6a875b87be23f02ba
lopezcl <lopezcl@mail.uc.edu>
2019-06-04 16:02:05 -0400

    created seperate pages for name screen and game screen

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const startButton = document.getElementById("startButton");
+
+const nameScreen = document.getElementById("nameScreen");
+const gameScreen = document.getElementById("gameScreen");
+
+startButton.addEventListener("click", function (startButtonEvent){
+
+    startButtonEvent.preventDefault();
+
+    // console.log(`something`);
+    
+    nameScreen.classList.add("hidden");
+
+    gameScreen.classList.remove("hidden");
+});
