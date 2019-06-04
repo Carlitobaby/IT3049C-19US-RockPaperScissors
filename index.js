4476529fe9c0ee675408faad5805a6fc1c542629
lopezcl <lopezcl@mail.uc.edu>
2019-06-04 16:46:14 -0400

    added getComputerChoice function to return a random value each time

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const startButton = document.getElementById("startButton");
 const nameScreen = document.getElementById("nameScreen");
 const gameScreen = document.getElementById("gameScreen");
 
+const gameForm = document.getElementById("gameForm");
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomChoice = Math.floor(Math.random() * 3);
+    return choices[randomChoice];
+}
+// console.log(getComputerChoice());
+
 startButton.addEventListener("click", function (startButtonEvent){
 
     startButtonEvent.preventDefault();
@@ -14,4 +23,8 @@ startButton.addEventListener("click", function (startButtonEvent){
     nameScreen.classList.add("hidden");
 
     gameScreen.classList.remove("hidden");
+});
+
+gameForm.addEventListener("submit", function(gameSubmitEvent){
+    gameSubmitEvent.preventDefault();
 });
