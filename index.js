1d339969a5667ca9a877065ca9f35b8955ea14e0
lopezcl <lopezcl@mail.uc.edu>
2019-06-09 01:21:02 -0400

    added userInput function to return user input from dropdown selection

--- index.js
+++ index.js
@@ -12,13 +12,18 @@ function getComputerChoice() {
     const randomChoice = Math.floor(Math.random() * 3);
     return choices[randomChoice];
 }
-// console.log(getComputerChoice());
+
+function userInput() {
+   const userChoice = document.getElementById("userValue");
+   var result = userChoice.options[userChoice.selectedIndex].value;
+   return result;
+}
 
 startButton.addEventListener("click", function (startButtonEvent){
 
     startButtonEvent.preventDefault();
 
-    // console.log(`something`);
+    // console.log(`start button event`);
     
     nameScreen.classList.add("hidden");
 
@@ -27,4 +32,6 @@ startButton.addEventListener("click", function (startButtonEvent){
 
 gameForm.addEventListener("submit", function(gameSubmitEvent){
     gameSubmitEvent.preventDefault();
+    // console.log(`computer choice:`, getComputerChoice());
+    // console.log(`user choice:`, userInput());
 });
